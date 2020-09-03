3e24406ee1a0a36f255c3d26bbfe5a3e95385c42
roybossofpesto <andreduracell@gmail.com>
2020-09-03 02:54:25 +0200

    shuffle

--- js/app.js
+++ js/app.js
@@ -9,6 +9,18 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
 console.log("app started")
 
 const scene = new THREE.Scene();
@@ -416,6 +428,8 @@ document.onkeydown = (event) => {
         is_animated = !is_animated;
     } else if (keyCode == 82) { // rr
         reset();
+    } else if (keyCode == 83) { // ss
+        shuffle(notes);
     } else if (keyCode == 76) { // ll
         display_all = !display_all;
     }
